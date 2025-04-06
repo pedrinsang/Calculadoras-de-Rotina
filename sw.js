62d1fdcae953cbecd35108d130fcaa7e3a3a4a6f
pedrinsang <126923958+pedrinsang@users.noreply.github.com>
2025-04-06 00:37:45 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'virologia-cache-v15'; // Atualizei a versão
+const CACHE_NAME = 'virologia-cache-v16'; // Atualizei a versão
 const OFFLINE_PAGE = './offline.html';
 const ASSETS = [
   './',
